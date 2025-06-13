6ca6caa7243bebcd8ef37c6689d7f9179a5ef80d
Striatp <sebastien.works@outlook.com>
2025-06-13 15:35:06 +0200

    Prettier configuration.

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+export const semi = true;
+export const singleQuote = true;
+export const printWidth = 100;
+export const trailingComma = "all";
+export const bracketSpacing = true;
+export const arrowParens = "always";
+export const endOfLine = "lf";
